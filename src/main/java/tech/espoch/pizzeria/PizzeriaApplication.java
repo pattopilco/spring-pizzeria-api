85efd2e2fe7db0de4243ea083d04ca293445b19e
pattopilco <pattopilco@gmail.com>
2023-12-16 15:21:53 -0500

    first commit

---
+++ src/main/java/tech/espoch/pizzeria/PizzeriaApplication.java
@@ -0,0 +1,13 @@
+package tech.espoch.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaApplication.class, args);
+	}
+
+}
