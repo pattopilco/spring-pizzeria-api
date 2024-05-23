87432405fc1dfde826d8dbef28a6715ba5e9871c
Patricio Pilco <pattopilco@gmail.com>
2024-05-22 21:44:36 -0500

    properties

--- src/test/java/rest/xpoch/pizzeria/PizzeriaApplicationTests.java
+++ src/test/java/rest/xpoch/pizzeria/PizzeriaApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class PizzeriaApplicationTests {
 
     @Test
